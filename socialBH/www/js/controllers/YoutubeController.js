944b081dccab6d49dff016f65cd1aed05180eb32
Luiz Santos <lsantosdutra@gmail.com>
2015-10-25 11:12:24 -0200

    Buscando video do canal da pbh

--- socialBH/www/js/controllers/YoutubeController.js
+++ socialBH/www/js/controllers/YoutubeController.js
@@ -1,7 +1,14 @@
 angular.module('starter.youtube', [])
 
-.controller('YoutubeController', function($scope, $state) {
+.controller('YoutubeController', function($scope, $state, $http) {
 
-	console.log('trace - YoutubeController');
+
+$http.get("https://www.googleapis.com/youtube/v3/search?key=AIzaSyB-dQu1U88WXAsHWP2JAgSijQM6RnT1yFM&channelId=UCDH88MpQ8HqvdzRegz12dew&part=snippet,id&order=date&maxResults=20")
+.then(function(response) {
+	$scope.lista = response.data.items;
+
+	console.log($scope.lista);
+
+})
 
 })
