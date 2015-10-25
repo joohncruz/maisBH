ba1bb81436be746460405648e0ad9732a192b8f3
Luiz Santos <lsantosdutra@gmail.com>
2015-10-25 12:03:26 -0200

    Buscando video e inserindo no scope

--- socialBH/www/js/controllers/FacebookController.js
+++ socialBH/www/js/controllers/FacebookController.js
@@ -4,7 +4,11 @@ angular.module('starter.facebook', [])
 
 	$http.get("https://graph.facebook.com/oauth/access_token?client_id=1489197401401789&client_secret=142ec263ac05784f9cb9e4b5db6bbfc8&grant_type=client_credentials")
 			.then(function(response) {
-			  console.log("peguei a chave" + response.data);
+			  var urlTimeLine = "https://graph.facebook.com/prefeiturabh/posts?fields=object_id,link,icon,source,picture,id,type,message&" + response.data;
+			  $http.get(urlTimeLine).then(function(resp) {
+			      $scope.lista = resp.data.data;
+			      console.log($scope.lista);
+			   })
 			})
 
 })
