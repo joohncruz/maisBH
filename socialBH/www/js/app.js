3ebae741d67608ab06603aacfbf3366384a3577e
Jonathan Cruz Rocha <joohncruzrocha@gmail.com>
2015-10-25 05:23:08 -0700

    Criando as Rotas

--- socialBH/www/js/app.js
+++ socialBH/www/js/app.js
@@ -11,3 +11,58 @@ angular.module('starter', ['ionic','starter.facebook', 'starter.instagram', 'sta
     }
   });
 })
+
+.config(function($stateProvider, $urlRouterProvider, $httpProvider) {
+
+  $stateProvider
+
+  .state('app', {
+    url: '/app',
+    abstract: true,
+    templateUrl: 'templates/menu.html',
+    controller: 'MenuController'
+  })
+
+  .state('app.main', {
+    url: '/main',
+    views: {
+      'content': {
+        templateUrl: 'templates/main.html',
+        controller: 'MainController'
+      }
+    }
+  })
+
+  .state('app.facebook', {
+    url: '/facebook',
+    views: {
+      'content': {
+        templateUrl: 'templates/facebook.html',
+        controller: 'FacebookController'
+      }
+    }
+  })
+
+  .state('app.instagram', {
+    url: '/instagram',
+    views: {
+      'content': {
+        templateUrl: 'templates/instagram.html',
+        controller: 'InstagramController'
+      }
+    }
+  })
+
+  .state('app.twitter', {
+    url: '/twitter',
+    views: {
+      'content': {
+        templateUrl: 'templates/twitter.html',
+        controller: 'TwitterController'
+      }
+    }
+  })
+
+  $urlRouterProvider.otherwise('/app/main');
+
+})
