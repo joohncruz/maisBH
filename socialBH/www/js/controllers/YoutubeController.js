36954e5e0b94e43f2243ba94fe93c0403ca0e4c9
Luiz Santos <lsantosdutra@gmail.com>
2015-10-25 11:21:35 -0200

    Fazendo função e abrir video

--- socialBH/www/js/controllers/YoutubeController.js
+++ socialBH/www/js/controllers/YoutubeController.js
@@ -1,14 +1,13 @@
 angular.module('starter.youtube', [])
 
 .controller('YoutubeController', function($scope, $state, $http) {
-
-
-$http.get("https://www.googleapis.com/youtube/v3/search?key=AIzaSyB-dQu1U88WXAsHWP2JAgSijQM6RnT1yFM&channelId=UCDH88MpQ8HqvdzRegz12dew&part=snippet,id&order=date&maxResults=20")
-.then(function(response) {
-	$scope.lista = response.data.items;
-
-	console.log($scope.lista);
-
-})
-
+	$http.get("https://www.googleapis.com/youtube/v3/search?key=AIzaSyB-dQu1U88WXAsHWP2JAgSijQM6RnT1yFM&channelId=UCDH88MpQ8HqvdzRegz12dew&part=snippet,id&order=date&maxResults=20")
+	.then(function(response) {
+		$scope.abrirVideo = function(idVideo) {
+		    //console.log('Abrir video: ' + idVideo)
+		    window.open('https://m.youtube.com/watch?v=' + idVideo, '_system', 'location=yes');
+		    return false;
+	  	}
+		$scope.lista = response.data.items;
+	})
 })
