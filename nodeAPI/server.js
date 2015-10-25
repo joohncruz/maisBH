0d045bbb00b4fb689f16c985816c9fe125fbe1a6
Jonathan Cruz Rocha <joohncruzrocha@gmail.com>
2015-10-25 08:30:52 -0700

    Rota test de get

---
+++ nodeAPI/server.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var app = express();
+
+var port = process.env.PORT || 3000;
+var router = express.Router(); 
+
+router.use(function(req, res, next){ 
+	console.log('Acesso...');
+	next();
+});
+
+router.get('/', function(req, res) { 
+	res.json({ message: 'TESTE' });
+});
+
+
+app.use('/api', router); 
+
+app.listen(port); 
+
+console.log('Escutando na ' + port);
