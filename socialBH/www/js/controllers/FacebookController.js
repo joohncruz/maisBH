aa25f96f38ba9c0996878ca6ecaf124af0fa7fe8
Luiz Santos <lsantosdutra@gmail.com>
2015-10-25 12:00:13 -0200

    Agora peguei a chave na api do facebook

--- socialBH/www/js/controllers/FacebookController.js
+++ socialBH/www/js/controllers/FacebookController.js
@@ -1,7 +1,10 @@
 angular.module('starter.facebook', [])
 
-.controller('FacebookController', function($scope, $state) {
+.controller('FacebookController', function($scope, $state,$http) {
 
-	console.log('trace - FacebookController');
+	$http.get("https://graph.facebook.com/oauth/access_token?client_id=1489197401401789&client_secret=142ec263ac05784f9cb9e4b5db6bbfc8&grant_type=client_credentials")
+			.then(function(response) {
+			  console.log("peguei a chave" + response.data);
+			})
 
 })
